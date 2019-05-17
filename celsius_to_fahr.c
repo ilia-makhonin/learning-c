e985fcbfd93bb2258b5f54c00dbfc7a33146ced3
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-17 23:04:16 +0300

    Updated all files

---
+++ celsius_to_fahr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
+
+int main()
+{
+	int celsius;
+	for (celsius = LOWER; celsius <= UPPER; celsius = celsius + STEP)
+	{
+		printf("%d %6.2f\n", celsius, (celsius * 9.0 / 5.0 + 32.0));
+	}
+	return 0;
+}
